4bff1a38e605512805d2bdeb0af2e2aa75c5eb18
Samantha Quiñones <samantha.quinones@teamaol.com>
2014-12-13 03:51:36 -0500

    Heroku lol

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 var async      = require('async');
 var gramophone = require('gramophone');
 var JoindinApi = require('./lib/joindin');
+var express    = require('express');
 
-var options = {
-    proxy: 'localhost:9292'
-};
-
-var api = new JoindinApi(options);
-
-async.waterfall(
-    [
-        function(callback) {
-            api.getUser('lornajane', function (data) {
-                var segments = data.uri.split('/');
-                var userid   = segments[segments.length-1];
-                callback(null, userid);
-            });
-        },
-        function(user_id, callback) {
-            api.getTalks(user_id, function (talks) {
-                var talk_ids = talks.map(function (talk) {
-                    var segments = talk.comments_uri.split('/');
-                    return segments[segments.length-2];
+var app = express();
+app.get('/:username', function(req, res) {
+    var api = new JoindinApi();
+    var username = req.params.username;
+    async.waterfall(
+        [
+            function(callback) {
+                api.getUser(username, function (data) {
+                    if (typeof data == 'undefined') {
+                        callback('No data');
+                        return;
+                    }
+                    var segments = data.uri.split('/');
+                    var userid   = segments[segments.length-1];
+                    callback(null, userid);
                 });
-                callback(null, talk_ids)
-            });
-        },
-        function(talk_ids, callback) {
-            api.getComments(talk_ids, function(comments) {
-                callback(null, comments.join());
-            })
-        },
-        function(comments, callback) {
-            var results = gramophone.extract(comments, {score: true});
-            callback(null, results);
+            },
+            function(user_id, callback) {
+                api.getTalks(user_id, function (talks) {
+                    var talk_ids = talks.map(function (talk) {
+                        var segments = talk.comments_uri.split('/');
+                        return segments[segments.length-2];
+                    });
+                    callback(null, talk_ids)
+                });
+            },
+            function(talk_ids, callback) {
+                api.getComments(talk_ids, function(comments) {
+                    callback(null, comments.join());
+                })
+            },
+            function(comments, callback) {
+                var results = gramophone.extract(comments, {score: true});
+                callback(null, results);
+            }
+        ],
+        function(err, results) {
+            res.send(JSON.stringify(results));
         }
-    ],
-    function(err, results) {
-        console.log(results);
-    }
-);
+    );
+});
+
+app.listen(process.argv[2]);
